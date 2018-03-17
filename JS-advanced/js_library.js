bfd7ceb4e0263df6823f3d422a5ad53260139d40
Greg Weber <gregoryweberguitars@gmail.com>
2018-03-16 22:27:58 -0700

    added reduce, find, reject

--- JS-advanced/js_library.js
+++ JS-advanced/js_library.js
@@ -11,7 +11,7 @@
 
 var _ = {
     //map takes in either an array or object
-   map: function(object, callback) {
+    map: function(object, callback) {
           let result = [];
           //  check to see if object is an array
           if (Array.isArray(object)){
@@ -26,17 +26,31 @@ var _ = {
           return result;
         },
 
-   reduce: function(array, callback, memo) { 
-    
+  
+    reduce: function(array, callback, memo) { 
+              let index = 0
+              //if memo doesnt get passed then set first index in array to memo and set the variable index to one so the for loop will start at the correct place
+                if (!memo){
+                  memo = array[0];
+                  index = 1;
+                }
+                //loop through the array and send memo and the current index value into the callback the return of which will incrment memo
+                for (; index < array.length; index++){
+                  memo = callback(memo, array[index]);
+                }
+                return memo
+            },
 
-   },
+    find: function(array, callback) {   
+            for (let element of array) {
+              if (callback(element)){
+                return element;
+              }
+            }
+            return undefined;
+          },
 
-   find: function() {   
-     // code here;
-
-   },
-
-   filter: function(array, callback) { 
+    filter: function(array, callback) { 
               let result = [];
               for (let element of array){
                 if (callback(element)){
@@ -46,11 +60,17 @@ var _ = {
               return result;
             },
 
-   reject: function() { 
-     // code here;
-   }
+    reject: function(array, callback){ 
+              let result = [];
+              for (let element of array){
+                if (!(callback(element))){
+                  result.push(element);
+                }
+              }
+              return result;
+            }
  }
-// you just created a library with 5 methods!
+
 
 
 // _.filter the return of the function getting passed to .filter here evaluates to a boolean ((try it, add a number to the function and you will 
@@ -67,10 +87,28 @@ var  objTimes3 = _.map({one: 1, two: 2, three: 3}, function(num, key){ return nu
 console.log(objTimes3);
 
 
-// reduce_.reduce(list, iteratee, [memo], [context]) Aliases: inject, foldl 
-// Also known as inject and foldl, reduce boils down a list of values into a single value. Memo is the initial state of the reduction, and each successive step of it should be returned by iteratee. The iteratee is passed four arguments: the memo, then the value and index (or key) of the iteration, and finally a reference to the entire list.
 
+// reduce_.reduce(list, iteratee, [memo], [context]) Aliases: inject, foldl 
+var sum = _.reduce([1, 2, 3], function(memo, num){ return memo + num; },0); // memo is 0 index => 6
+console.log(sum);
+var sum2 = _.reduce([1, 2, 3], function(memo, num){ return memo + num; },10); //memo is 10 => 16
+console.log(sum2);
+var sum3 = _.reduce([1, 2, 3], function(memo, num){ return memo + num; }); // no memo this time => 6
+console.log(sum3);
+// Also known as inject and fold, reduce boils down a list of values into a single value. Memo is the initial state of the reduction, and each successive step of it should be returned by iteratee. The iteratee is passed four arguments: the memo, then the value and index (or key) of the iteration, and finally a reference to the entire list.
 // If no memo is passed to the initial invocation of reduce, the iteratee is not invoked on the first element of the list. The first element is instead passed as the memo in the invocation of the iteratee on the next element in the list.
 
-// var sum = _.reduce([1, 2, 3], function(memo, num){ return memo + num; }, 0);
-// => 6
+
+
+// _.find(list, predicate, [context]) Alias: detect 
+// Looks through each value in the list, returning the first one that passes a truth test (predicate), or undefined if no value passes the test. The function returns as soon as it finds an acceptable element, and doesn't traverse the entire list.
+var even = _.find([1, 3, 4, 5, 7], function(num){ return num % 2 == 0; }); // => 4
+var even2= _.find([1, 3, 5, 7], function(num){ return num % 2 == 0; }); // => undefined
+console.log(even);
+console.log(even2);
+
+
+// _.reject(list, predicate, [context]) 
+// Returns the values in list without the elements that the truth test (predicate) passes. The opposite of filter.
+var odds = _.reject([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });  // => [1, 3, 5]
+console.log(odds);
