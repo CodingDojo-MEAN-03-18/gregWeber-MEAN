10efca44c51e78f492bd8f37568a95f67996338b
Greg Weber <gregoryweberguitars@gmail.com>
2018-03-20 11:35:25 -0700

    got numOrders to talk to receivedItem function so now it will calculate the number of orders in a call to the wearhouse automatically

--- orderSuppliesChallenge.js
+++ orderSuppliesChallenge.js
@@ -1,6 +1,5 @@
 
 
-
 function orderSupplies(orderId, item, callback) {
     let warehouse; //undefined
     const deliveryTime = Math.random() * 3000;
@@ -23,19 +22,19 @@ function orderSupplies(orderId, item, callback) {
 //tracks received items /sorts received order/ console.logs the returns in the correct order 
 //takes in number of items ordered as argument
 (function orderTracker(numOrders){
+    //for some reason _ couldn't get numOrders to pass into receivedItem function untill I did 'this' here and used this.numOrders in the conditional
+    this.numOrders = numOrders;
     let items = [];
-    // let numOrders = 0
+
     function receivedItem(orderId, item){
         //pushes an object to items array
         items.push({ orderId, 
             'item' : `Received ${item.product} time to ${item.directions()}`
         });
-        // console.log(numOrders);
-        // console.log(items.length === numOrders, items.length, numOrders);
-        // console.log(numOrders);
-            if (items.length === numOrders){
+
+        if (items.length === this.numOrders){
+            // console.log(items.length === this.numOrders);
                 //get order correct now that the order has been filled -- sort by order id
-                // console.log(items)
                 let temp;
                 let needsSort = true;
                 while(needsSort){
@@ -63,42 +62,28 @@ function orderSupplies(orderId, item, callback) {
 
 
 // ordering instructions:
-//  when supplies are ordered give them a sequential id --> match up when we recieve to get correct order of expected return.
+// when supplies are ordered give them a sequential id --> match up when we recieve to get correct order of expected return.
 // function orderTracker takes a quantity of the order about to be placed and wraps around the receivedItem function.
 // by invoking orderTracker, recievedItem gets exposed to global scope.
 
-orderTracker(2);
-orderSupplies(0, 'paint', receivedItem);
-orderSupplies(1, 'brush', receivedItem);
+// helper function that compiles the list of orders and sends to the orderSupplies warehouse | also generates number of orders to 
+// be expected so we can stage the returns and deliver them all once the porder is full
+function orderMaker(...orders){
+    console.log(orders.length, 'in orderMaker');
+    // call the wearhouse and place order;
+    function placeOrder(orderList){
+        for (order of orderList){;
+            // console.log(order);
+            orderSupplies(order[0], order[1], order[2]);
+        }
+    }
+    orderTracker(orders.length);
+    placeOrder(orders);
+}
 
 
-orderTracker(5);
-orderSupplies(1, 'paint', receivedItem);
-orderSupplies(2, 'brush', receivedItem);
-orderSupplies(3, 'brush', receivedItem);
-orderSupplies(4, 'brush', receivedItem);
-orderSupplies(5, 'brush', receivedItem);
+// place order  as an array with 3 values each ( ...orders) needs order id for correct ordering for delivery
+orderMaker([1, 'paint', receivedItem], [2, 'brush', receivedItem]);
+// orderMaker([1, 'paint', receivedItem], [2, 'brush', receivedItem], [3, 'brush', receivedItem]);
+// orderMaker([1, 'paint', receivedItem], [2, 'paint', receivedItem], [3, 'brush', receivedItem], [4, 'brush', receivedItem], [5, 'brush', receivedItem], [6, 'brush', receivedItem], );
 
-//helper function that compiles the list of orders and sends to the orderSupplies warehouse | numOfOrders isn't necessarily a quantity 
-// - that could be added if you wanted to do something with displaying a quantity or kkeeping track of inventory. numOrders is 1 num per order.
-// function orderMaker(...orders){
-//     // call the wearhouse and place order;
-//     function placeOrder(orderList){
-//         for (order of orderList){;
-//             // console.log(order);
-//             orderSupplies(order[0], order[1], order[2]);
-//         }
-//     }
-//     // console.log(orderTracker);
-//     //call ordertracker to tell them to be on the look out for upcoming order returns
-//     this.orderLength = orders.length;
-//     // console.log(orders.length);
-//     orderTracker(2);
-//     // console.log(orders);
-//     placeOrder(orders);
-// }
-
-
-
-// // place order (numOrders, ...orders)
-// orderMaker([1, 'paint', receivedItem], [2, 'brush', receivedItem]);
