a57ded214190db050d1feaf86708afb720f45f9d
Greg Weber <gregoryweberguitars@gmail.com>
2018-03-22 12:23:03 -0700

    moved product id to warehouse and refactored

--- orderSuppliesChallenge.js
+++ orderSuppliesChallenge.js
@@ -1,6 +1,6 @@
 
 
-function orderSupplies(orderId, item, callback) {
+function orderSupplies(item, callback) {
     //put in error handeling here for items ordered that are not carried or out of
 
     let warehouse; //undefined
@@ -8,15 +8,27 @@ function orderSupplies(orderId, item, callback) {
     setTimeout(function() {
       warehouse = {
         paint: {
-          product: 'Neon Green Paint',
-          directions: function() { return 'mix it!' }
+            id : 1,
+            product: 'Neon Green Paint',
+            directions: function() { return 'mix it!' }
         },
         brush: {
-          product: 'Horsehair brush',
-          directions: function() { return 'start painting!' }
-        }
+            id:2,
+            product: 'Horsehair brush',
+            directions: function() { return 'start painting!' }
+        },
+        // beer: {
+        //     id : 3,
+        //     product: 'Craft IPA',
+        //     directions: () => 'open ang enjoy'
+        // },
+        // pizza: {
+        //     id : 4,
+        //     product: "Savory delicious pizza",
+        //     directions: () => 'take a break and have a slice'
+        // }
       };
-      callback(orderId, warehouse[item]);
+      callback(warehouse[item]);
     }, deliveryTime);
   }
 
@@ -24,17 +36,17 @@ function orderSupplies(orderId, item, callback) {
 //tracks received items /sorts received order/ console.logs the returns in the correct order 
 //takes in number of items ordered as argument
 (function orderTracker(numOrders){
-    //for some reason _ couldn't get numOrders to pass into receivedItem function untill I did 'this' here and used this.numOrders in the conditional
+    // for some reason _ couldn't get numOrders to pass into receivedItem function untill I did 'this' here and used this.numOrders in the conditional
     this.numOrders = numOrders;
     let items = [];
 
     //better would be to sort them as we receive them with an insert at value 1 lower than or something like that, the way I do it below works well for short lists
 
-    function receivedItem(orderId, item){
+    function receivedItem(item){
         //pushes an object to items array
         items.push({ 
-            orderId,
-            item : `Received ${item.product} time to ${item.directions()}`
+            id : item.id,
+            message : `Received ${item.product} time to ${item.directions()}`
         });
         // console.log(item);
 
@@ -46,7 +58,7 @@ function orderSupplies(orderId, item, callback) {
                 while(needsSort){
                     needsSort = false; 
                     for (index = 0; index < items.length-1; index++){
-                        if (items[index].orderId > items[index + 1].orderId){
+                        if (items[index].id > items[index + 1].id){
                             needsSort = true;
                             //swap
                             temp = items[index];
@@ -57,7 +69,7 @@ function orderSupplies(orderId, item, callback) {
                 }
                 // now that the order is correct we can deliver the items
                 for (element of items){
-                console.log(element.item);
+                console.log(element.message);
                 }
             }
         }
@@ -68,8 +80,6 @@ function orderSupplies(orderId, item, callback) {
 
 
 // ordering instructions:
-// when supplies are ordered give them a sequential id --> match up when we recieve to get correct order of expected return.
-// function orderTracker takes a quantity of the order about to be placed and wraps around the receivedItem function.
 // by invoking orderTracker, recievedItem gets exposed to global scope.
 
 // helper function that compiles the list of orders and sends to the orderSupplies warehouse | also generates number of orders to 
@@ -93,7 +103,9 @@ function orderMaker(...orders){
 // place order  as an array with 3 values each ( ...orders) needs order id for correct ordering for delivery
 //would be better if the projucts had id's in the warehouse...
 
-orderMaker([1, 'paint', receivedItem], [2, 'brush', receivedItem]);
+orderMaker(['paint', receivedItem], ['brush', receivedItem]);
+// orderMaker(['brush', receivedItem], ['paint', receivedItem]);
+// orderMaker(['paint', receivedItem], ['brush', receivedItem], ['beer', receivedItem]);
 
-// orderMaker( [4, 'pizza', receivedItem], [3, 'beer', receivedItem], [1, 'paint', receivedItem], [2, 'brush', receivedItem] );
+// orderMaker( ['pizza', receivedItem], ['beer', receivedItem], ['paint', receivedItem], ['brush', receivedItem] );
 
