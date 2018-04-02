d6b46e8711a8a5013cda122af70a48ce02f6516a
Greg Weber <gregoryweberguitars@gmail.com>
2018-04-01 18:19:37 -0700

    added messaging - also removes all the users  messages from everyone when the user leaves the chat

--- Express/Socket.io/group_chat/server.js
+++ Express/Socket.io/group_chat/server.js
@@ -18,23 +18,28 @@ const server = app.listen(port, ()=> console.log(`listening on ${port}`));
 const io = require('socket.io').listen(server); 
 
 const users = {}; //stores users by their id
-const messages = []; //for message objects[{id:socket.id, name:name, message:message}, {...}, {...}]
+const allMessages = []; //for message objects[{id:socket.id, name:name, message:message}, {...}, {...}]
 io.sockets.on('connection', socket=> {
     console.log('connected on surver side')
     socket.on('new_user', data => {
         console.log('new user joined named: ', data.name , socket.id);
         //add new user to the users object by id
         users[socket.id] = data.name;
+        //brodcast to all about the new user - except new user
         socket.broadcast.emit('user_added', {name: data.name, id: socket.id})
-        console.log(users);
+        //send new user all current messages
+        socket.emit('prevMessages', { allMessages })
+
     });
 
-//add new users to a users object so we can get their name by id when creating new messages
-//add a messages object to store messages by id;
-// send messages over as they're created, send all live messages when new user joins
-//delet messages by id from all messages when a user disconnects
-//on client, display the messages to new users and add method to append new messages to current users
-
+    socket.on('newMessage', data => {
+        let newMessageData = {message : data.message,  name: users[socket.id], id:socket.id};
+        // send back for all users
+        io.emit('addMessage', newMessageData);
+        //add to allMessages array
+        allMessages.push(newMessageData);
+        console.log('all messages: ', allMessages);
+    })
 
 
 
@@ -43,8 +48,11 @@ io.sockets.on('connection', socket=> {
         io.emit('user_left', { id: socket.id})
         //remove user from the user object on disconnect - stored by socket.id
         delete users[socket.id];
-    
         //remove this users messages from the message array on disconnect - loop thru and delete on id match
+        allMessages.forEach( (element, index) => {
+            if (element.id === socket.id){allMessages.splice(index, 1)};
+        })
+        console.log('messages now:', allMessages)
 
     });
 });
