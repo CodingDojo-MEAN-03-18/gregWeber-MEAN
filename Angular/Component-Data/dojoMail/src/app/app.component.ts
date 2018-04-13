904b6f01e2722bda0cab30616d6759929a216fdd
Greg Weber <gregoryweberguitars@gmail.com>
2018-04-13 14:12:52 -0700

    using directives interpolation and pipes to create a table from an object of emails

---
+++ Angular/Component-Data/dojoMail/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Dojo Mail';
+  emails: ({email: string, importance: boolean, subject: string, content: string})[] = [
+    {email: 'bill@gates.com',
+      importance: true,
+      subject: 'New Windows',
+      content: 'Windows XI will launch in year 2100',
+    },
+    {email: 'ADA@lovelace.com',
+    importance: true,
+    subject: 'Programming',
+    content: 'Enchantress of numbers',
+    },
+    {email: 'john@carmac.com',
+    importance: false,
+    subject: 'Updated Algo',
+    content: 'New algorithm for shadow volumes',
+    },
+    {email: 'GABE@newel.com',
+    importance: false,
+    subject: 'HL3',
+    content: 'Just kiddding....',
+    },
+  ];
+}
