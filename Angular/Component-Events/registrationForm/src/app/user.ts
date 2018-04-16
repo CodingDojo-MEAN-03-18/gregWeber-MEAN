e483ba9f87a0213abcb6bf4543d4b33cbf7b7736
Greg Weber <gregoryweberguitars@gmail.com>
2018-04-15 22:59:53 -0700

    registration form using two-way binding for validation and ngIf to display error messages

---
+++ Angular/Component-Events/registrationForm/src/app/user.ts
@@ -0,0 +1,15 @@
+
+
+export class User {
+  constructor(
+    first_name: string = '',
+    last_name: string = '',
+    email: string = '',
+    password: string = '',
+    street: string = '',
+    apt: number = null,
+    city: string = '',
+    state: string = '',
+    lucky: string  = '',
+  ) {}
+}
