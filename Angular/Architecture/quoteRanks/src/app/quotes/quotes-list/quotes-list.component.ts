6c12a2d046f2cab510871a700198ce42824bba16
Greg Weber <gregoryweberguitars@gmail.com>
2018-04-19 21:51:15 -0700

    voting and delete done, now just need the sorting of the quotes upon vote change

---
+++ Angular/Architecture/quoteRanks/src/app/quotes/quotes-list/quotes-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-quotes-list',
+  templateUrl: './quotes-list.component.html',
+  styleUrls: ['./quotes-list.component.css']
+})
+export class QuotesListComponent {
+  // recieve quotes array data
+  @Input() quoteList: Quote[];
+  // send voteCount/deleteQuote data to parent
+  @Output() onVoteUp = new EventEmitter();
+
+  constructor() { }
+
+  voteUp(quote: Quote) {
+    quote.voteCount++;
+  }
+  voteDown(quote: Quote) {
+    quote.voteCount--;
+  }
+  deleteQuote(quote: Quote) {
+    this.quoteList = this.quoteList.filter(q => q !== quote);
+  }
+
+}
