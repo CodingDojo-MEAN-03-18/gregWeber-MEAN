790b01b433289277e6585c582c02645c5e6b43d9
Greg Weber <gregoryweberguitars@gmail.com>
2018-04-29 14:52:01 -0700

    using behavior subject, filter, routing with params

---
+++ Angular/Routing/productManagment/src/app/product.ts
@@ -0,0 +1,12 @@
+import { identifierModuleUrl } from "@angular/compiler";
+
+export class Product {
+
+  constructor(
+    public id = Math.floor(Math.random() * 1000),
+    public title = '',
+    public price = 0,
+    // public imgUrl = '',
+  ) { }
+}
+
